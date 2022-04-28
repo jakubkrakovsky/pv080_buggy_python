b21c24344eaefb4a5db5d1ac9f35ee5366f944b0
jakubkrakovsky <104575670+jakubkrakovsky@users.noreply.github.com>
2022-04-28 16:42:39 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
-import yaml
-import subprocess
 import hashlib
 import subprocess
+import yaml
 import flask
 
 
@@ -28,8 +27,8 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
     http = urllib.PoolManager()
-    r = http.request('GET', url)
-    return r.data
+    r_snake = http.request('GET', url)
+    return r_snake.data
 
 
 @app.route("/")
